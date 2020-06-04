bdf48f24d09c42847bd7af012c8fd7a7cd103e83
Darren <dangus1924@gmail.com>
2020-06-04 19:25:44 -0400

    created branch

--- src/05_lists.py
+++ src/05_lists.py
@@ -9,21 +9,26 @@ y = [8, 9, 10]
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
 print(x)
-
+x += [4]
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
 print(x)
-
+x += [5]
+x += y
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
 print(x)
-
+x.pop(4)
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
 print(x)
-
+x += [99]
+print(x)
 # Print the length of list x
 # YOUR CODE HERE
-
+print(len(x))
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+mupltipled_list = [element * 1000 for element in x]
+
+print(mupltipled_list)
--- src/06_tuples.py
+++ src/06_tuples.py
@@ -37,8 +37,11 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # YOUR CODE HERE
 
 t = (1, 2, 5, 7, 99)
-print_tuple(t)  # Prints 1 2 5 7 99, one per line
-
+def print_tuple(t):  # Prints 1 2 5 7 99, one per line
+    return
+print(print_tuple(1))
 # Declare a tuple of 1 element then print it
 u = (1)  # What needs to be added to make this work?
-print_tuple(u)
+def print_tuple(u):
+    return
+print(print_tuple(1))
